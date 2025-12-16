f1815051370138c4eda59108c88d6ba369500169
Lobato-py <lobatoamorim1@gmail.com>
2025-12-15 22:02:18 -0300

    aula1.py
    Refazendo todo o curso pq passei bom tempo sem estudar btw

---
+++ aula1.py
@@ -0,0 +1,13 @@
+# O símbolo #(hashtag) é utilizado para escrever comentários
+
+print('Hello World')
+
+# Na leitura de código, o VSCode lê sempre da esquerda para direita, e interpreta
+# a "#" como "comentários", ou seja, ignora a linha de código
+
+"""
+Quando utilizamos aspas tríplas, estamos usando DocStrings, que são como comentários
+porém o VSCode não os ignora completamente, ele lê mas não executa.
+
+LEMBRANDO: Se usou ", termina com "; Se usou ', termina com '.
+"""
