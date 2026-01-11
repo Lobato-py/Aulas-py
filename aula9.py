29fa9cd27a416f22d214857b9c1d2e6c68ac10f6
Lobato-py <lobatoamorim1@gmail.com>
2026-01-11 19:37:33 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,7 @@
+concatenacao = 'A' + 'B' + 'C' # O sinal de + na concatenação não consegue juntar str + int/float
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10 # Sinal de multiplicação sendo utilizado para repetição
+tres_vezes_gustavo = 3 * 'Gustavo'
+print(a_dez_vezes)
+print(tres_vezes_gustavo)
